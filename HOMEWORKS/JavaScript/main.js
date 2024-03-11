f73d6a685562e6b95a52409d7c5affa3fc1c7d97
Zinne0811 <nguyen.tinh.8113@gmail.com>
2024-03-12 01:03:54 +0700

    week 3 javascript 90% done

--- HOMEWORKS/JavaScript/main.js
+++ HOMEWORKS/JavaScript/main.js
@@ -7,8 +7,6 @@ let yearSelect = document.getElementById('year');
 
 // Lấy ngày, tháng và năm hiện tại
 let currentDate = new Date();
-let currentDay = currentDate.getDate();
-let currentMonth = currentDate.getMonth() + 1; // Tháng bắt đầu từ 0
 let currentYear = currentDate.getFullYear();
 
 // Đặt giá trị cho các select box
@@ -16,9 +14,6 @@ for (let i = 1; i <= 31; i++) {
     let option = document.createElement('option');
     option.value = i;
     option.text = i;
-    if (i === currentDay) {
-        option.selected = true;
-    }
     daySelect.appendChild(option);
 }
 
@@ -26,9 +21,6 @@ for (let i = 1; i <= 12; i++) {
     let option = document.createElement('option');
     option.value = i;
     option.text = 'Tháng ' + i;
-    if (i === currentMonth) {
-        option.selected = true;
-    }
     monthSelect.appendChild(option);
 }
 
@@ -36,9 +28,6 @@ for (let i = currentYear; i >= 1900; i--) {
     let option = document.createElement('option');
     option.value = i;
     option.text = i;
-    if (i === currentYear) {
-        option.selected = true;
-    }
     yearSelect.appendChild(option);
 }
 
@@ -47,6 +36,7 @@ for (let i = currentYear; i >= 1900; i--) {
 const customRadioButton = document.getElementById("custom");
 const customInput = document.getElementById("custom_input");
 const genderRadios = document.querySelectorAll('.gender');
+// console.log(genderRadios)
 
 customRadioButton.addEventListener("change", function () {
     if (this.checked) {
@@ -65,66 +55,140 @@ genderRadios.forEach(function (radio) {
 });
 
 
-// // Kiểm tra nhập tên, email, password chưa
-// formReg.ho.onfocus = function () {
-//     document.querySelectorAll('.error_mess').forEach(element => {
-//         element.style.display = 'none';
-//     });
-//     document.querySelector('.ho').style.display = 'inline-block';
-// }
-// formReg.ho.onblur = function() {
-//     if(this.value == '') this.classList.add("input_error");
-//     else this.classList.remove("input_error");
-// }
+// // Hiện ra gợi ý khi nhập tên, email, password 
+formReg.ho.onfocus = function () {
+    document.querySelectorAll('.error_mess').forEach(element => {
+        element.style.display = 'none';
+    });
+    document.querySelector('.ho').style.display = 'inline-block';
+}
+formReg.ho.onblur = function () {
+    document.querySelector('.ho').style.display = 'none';
+    if (this.value == '') this.classList.add("input_error");
+    else this.classList.remove("input_error");
+}
 
-// formReg.ten.onfocus = function () {
-//     document.querySelectorAll('.error_mess').forEach(element => {
-//         element.style.display = 'none';
-//     });
-//     document.querySelector('.ten').style.display = 'inline-block';
-// }
-// formReg.ten.onblur = function () {
-//     if (this.value == '') this.classList.add("input_error");
-//     else this.classList.remove("input_error");
-// }
+formReg.ten.onfocus = function () {
+    document.querySelectorAll('.error_mess').forEach(element => {
+        element.style.display = 'none';
+    });
+    document.querySelector('.ten').style.display = 'inline-block';
+}
+formReg.ten.onblur = function () {
+    document.querySelector('.ten').style.display = 'none';
+    if (this.value == '') this.classList.add("input_error");
+    else this.classList.remove("input_error");
+}
 
-// formReg.email.onfocus = function () {
-//     document.querySelectorAll('.error_mess').forEach(element => {
-//         element.style.display = 'none';
-//     });
-//     document.querySelector('.email').style.display = 'inline-block';
-// }
-// formReg.email.onblur = function () {
-//     if (this.value == '') this.classList.add("input_error");
-//     else this.classList.remove("input_error");
-// }
+formReg.email.onfocus = function () {
+    document.querySelectorAll('.error_mess').forEach(element => {
+        element.style.display = 'none';
+    });
+    document.querySelector('.email').style.display = 'inline-block';
+}
+formReg.email.onblur = function () {
+    document.querySelector('.email').style.display = 'none';
+    document.querySelector('.fail_email').style.display = 'none';
+    if (this.value == '') this.classList.add("input_error");
+    else this.classList.remove("input_error");
+}
 
-// formReg.pw.onfocus = function () {
-//     document.querySelectorAll('.error_mess').forEach(element => {
-//         element.style.display = 'none';
-//     });
-//     document.querySelector('.pw').style.display = 'inline-block';
-// }
-// formReg.pw.onblur = function () {
-//     if (this.value == '') this.classList.add("input_error");
-//     else this.classList.remove("input_error");
-// }
+formReg.pw.onfocus = function () {
+    document.querySelectorAll('.error_mess').forEach(element => {
+        element.style.display = 'none';
+    });
+    document.querySelector('.pw').style.display = 'inline-block';
+}
+formReg.pw.onblur = function () {
+    document.querySelector('.pw').style.display = 'none';
+    if (this.value == '') this.classList.add("input_error");
+    else this.classList.remove("input_error");
+}
 
-// formReg.ngay.onblur = function () {
-//     if (this.value == '') this.classList.add("input_error");
-//     else this.classList.remove("input_error");
-// }
 
 //Kiểm tra nhập đúng form chưa
 let email = document.getElementById("email");
-console.log(email);
+let password = document.getElementById("password")
+let Ho = document.getElementById("Ho");
+let Ten = document.getElementById("Ten");
+
+
 formReg.addEventListener("submit", (e) => {
     e.preventDefault();
 
     let phoneRegEx = /^[0-9]{10,11}$/;
-    let emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    let emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!phoneRegEx.test(email.value) && !emailRegEx.test(email.value))
+    // Kiểm tra input có đầy đủ không
+    if (Ho.value.trim() === "") {
+        document.querySelector('.ho').style.display = 'inline-block';
+    }
+    else if (Ten.value.trim() === "") {
+        document.querySelector('.ten').style.display = 'inline-block';
+    }
+    else if (email.value.trim() === "") {
+        document.querySelector('.email').style.display = 'inline-block';
+    }
+
+    else if (password.value.trim() === "") {
+        document.querySelector('.pw').style.display = 'inline-block';
+    }
+    if (Ho.value.trim() === "") {
+        Ho.classList.add("input_error");
+    }
+
+    if (Ten.value.trim() === "") {
+        Ten.classList.add("input_error");
+    }
+    if (email.value.trim() === "") {
+        email.classList.add("input_error");
+    }
+    if (password.value.trim() === "") {
+        password.classList.add("input_error");
+    }
+    if (phoneRegEx.test(email.value) === false && emailRegEx.test(email.value) === false)
         document.querySelector('.fail_email').style.display = 'inline-block';
 
+    if (daySelect.value === ""
+        || monthSelect.value === ""
+        || yearSelect.value === "") {
+        document.querySelector('.birthday').classList.add("input_error");
+        document.querySelector('.err_birthday').style.display = 'inline-block';
+    }
+    else if (daySelect.value !== "" || monthSelect.value !== "" || yearSelect.value !== "") {
+        document.querySelector('.birthday').classList.remove("input_error");
+        document.querySelector('.err_birthday').style.display = 'none';
+    }
+
+    let selectedGender = null;
+    genderRadios.forEach(function (radio) {
+        if (radio.checked) {
+            selectedGender = radio.id;
+        }
+    });
+    if (selectedGender === null) {
+        document.querySelector('.header_gender').classList.add("input_error");
+        document.querySelector('.err_gender').style.display = 'inline-block';
+    }
+    else{
+        document.querySelector('.header_gender').classList.remove("input_error");
+        document.querySelector('.err_gender').style.display = 'none';
+    }
+    let customSelect = document.getElementById('select_gender_custom');
+
+    if (selectedGender === "custom") {
+
+        if (customSelect.value === "0") {
+            document.querySelector('.err_customer').style.display = 'inline-block';
+            document.querySelector('.select_gender_custom').classList.add("input_error");
+        }
+    }
+
+    if (Ho.value.trim() !== "" && Ten.value.trim() !== ""
+        && (phoneRegEx.test(email.value) === true || emailRegEx.test(email.value) === true)
+        && (daySelect.value !== "" || monthSelect.value !== "" || yearSelect.value !== "")
+        && (selectedGender !== null)) {
+        if (selectedGender === "custom") { if (customSelect.value !== "0") alert("success!"); }
+        else alert("success!");
+    }
 })
